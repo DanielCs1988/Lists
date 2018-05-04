9b979ad31f674995ab9475d319f36b6f48547467
Emperor <daniel.cs20@gmail.com>
2018-05-04 16:03:25 +0200

    DynamicArray unit tests.

--- test/com/danielcs88/datastructures/DynamicArrayTest.java
+++ test/com/danielcs88/datastructures/DynamicArrayTest.java
@@ -1,15 +1,23 @@
 package com.danielcs88.datastructures;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DynamicArrayTest extends SinglyLinkedListTest {
+class DynamicArrayTest extends DoublyLinkedListTest {
 
     @BeforeEach
     private void fillTheList() {
         list = new DynamicArray<>(Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h", "i", "j"));
+    }
+
+    @Test
+    void testInsertAll() {
+        list.addAll(4, Arrays.asList("z", "q", "x"));
+        String expected = "a, b, c, d, z, q, x, e, f, g, h, i, j";
+        assertEquals(expected, list.toString());
     }
 }
